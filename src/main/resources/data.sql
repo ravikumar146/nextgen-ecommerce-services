7635afcb313fcdebcd5650927dbc15813731125b
Ravi Kumar <botcha.rav146@gmail.com>
2026-09-21 11:11:16 +0530

    Remove customer module and moved to seperate service

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -66,22 +66,6 @@ INSERT INTO orders
     (id, order_date, total_amount, status, customer_id)
 VALUES (2, CURRENT_TIMESTAMP, 150.00, 'Completed', 10002);
 
--- Customer
-INSERT INTO customers (id, customer_number, first_name, email, phone_number, status, active, created_at, updated_at, version)
-VALUES ('550e8400-e29b-41d4-a716-446655440000', 'CUST100001',
-        'John', 'john.doe@example.com', '123-456-7890', 'ACTIVE', true,
-        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 1);
-
--- Address
-INSERT INTO customer_addresses (id, customer_id, type, address_line1, address_line2,
-                                landmark, city, state, country, postal_code,
-                                active, default_address, created_at, updated_at, version)
-VALUES ('660e8400-e29b-41d4-a716-446655440000', '550e8400-e29b-41d4-a716-446655440000', 'HOME',
-        '123 Main Street', 'Apartment 4B', 'Near Central Park', 'New York', 'New York',
-        'USA', '10001', true, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP,
-        1);
-
-
 INSERT INTO cart_items
     (id, customer_id, product_ids)
 VALUES (101, 10001, ARRAY[1, 2]);
